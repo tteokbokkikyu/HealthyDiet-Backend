35119475b4958a11ab7530b3b1196ce9367fc7ce
sxm <59765936+windfall2321@users.noreply.github.com>
2025-06-15 15:05:14 +0800

    菜品识别api更新

--- src/main/java/com/hd/hd_backend/HdApplication.java
+++ src/main/java/com/hd/hd_backend/HdApplication.java
@@ -1,10 +1,12 @@
 package com.hd.hd_backend;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(scanBasePackages = {"com.hd.hd_backend"})
+@MapperScan("com.hd.hd_backend.mapper")
 public class HdApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
